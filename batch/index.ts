649fbb2ad8b6658a8502466bc89e146f663d302a
Evan <Evancat77@gmail.com>
2020-12-06 01:27:43 -0500

    task-splitter -> batch with improvements

---
+++ batch/index.ts
@@ -0,0 +1,53 @@
+const DEFAULT_UPDATE_INTERVAL = 0.25;
+
+/** Puts an array of items in batches for you to update over time instead of all at once */
+export default class Batcher<T> {
+	private currentIndex = 0;
+
+	private updatesPerSecond = 0;
+	private updatesPerFrame = 0;
+	private updateAccumlator = 0;
+
+	private items: Array<T>
+
+	/**
+	 * @param fetchFunc - you should return the items you want batched in this callback
+	 * @param updateInterval - interval at which items should be updated
+	 */
+	constructor(private fetchFunc: () => Array<T>, private updateInterval = DEFAULT_UPDATE_INTERVAL) {
+		this.items = fetchFunc()
+	}
+
+	/**
+	 * @param deltaTime - time between last batch.step(deltaTime) call and now
+	 * @returns an array of items to update
+	 */
+	public step(deltaTime: number) {
+		if (deltaTime === 0) {
+			return [];
+		}
+
+
+		this.updatesPerSecond = this.items.size() / this.updateInterval;
+		this.updatesPerFrame = this.updatesPerSecond * deltaTime;
+		this.updateAccumlator += this.updatesPerFrame;
+
+		const updateCount = math.min(math.floor(this.updateAccumlator), this.items.size());
+		this.updateAccumlator -= updateCount;
+
+		const batch: Array<T> = new Array(updateCount);
+
+		for (let i = 0; i < updateCount; i++) {
+			this.currentIndex += 1;
+
+			if (this.currentIndex >= this.items.size()) {
+				this.items = this.fetchFunc()
+				this.currentIndex = 0;
+			}
+
+			batch.push(this.items[this.currentIndex]);
+		}
+
+		return batch;
+	}
+}
